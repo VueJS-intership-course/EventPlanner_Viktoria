dfca3b2f62e6c7810f1ddcb0eef9eb4bbeccd968
Viktoria Dzivkova <viktoriad12@icloud.com>
2023-11-07 16:54:08 +0200

    add map to event details page

---
+++ src/components/MapDisplay.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div id="eventDetailsMap" style="width: 600px; height: 400px"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { Style, Circle, Fill } from "ol/style";
+import { fromLonLat } from "ol/proj";
+import "ol/ol.css";
+
+const { location } = defineProps({
+  location: {
+    type: Array,
+    required: true,
+  },
+});
+
+const map = ref(null);
+const vectorSource = ref(null);
+
+const initMap = () => {
+  const vectorSourceInstance = new VectorSource();
+  const vectorLayer = new VectorLayer({
+    source: vectorSourceInstance,
+  });
+
+  const mapInstance = new Map({
+    target: "eventDetailsMap",
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+      vectorLayer,
+    ],
+    view: new View({
+      center: fromLonLat(location),
+      zoom: 4,
+    }),
+  });
+
+  displayLocationPoint(location, vectorSourceInstance);
+
+  map.value = mapInstance;
+  vectorSource.value = vectorSourceInstance;
+};
+
+const displayLocationPoint = (lonLat, vectorSourceInstance) => {
+  const point = new Feature({
+    geometry: new Point(fromLonLat(lonLat)),
+  });
+
+  const pointStyle = new Style({
+    image: new Circle({
+      radius: 7,
+      fill: new Fill({
+        color: "blue",
+      }),
+    }),
+  });
+
+  point.setStyle(pointStyle);
+  vectorSourceInstance.clear();
+  vectorSourceInstance.addFeature(point);
+};
+
+onMounted(() => {
+  initMap();
+});
+
+onBeforeUnmount(() => {
+  if (map.value) {
+    map.value.setTarget(null);
+  }
+});
+</script>
--- src/pages/events-page/EventDetailsPage.vue
+++ src/pages/events-page/EventDetailsPage.vue
@@ -1,81 +1,96 @@
 <template>
-  <div v-if="isLoading">
-    <p>Loading event details...</p>
-  </div>
-  <div v-if="event" class="container my-4">
-    <div class="row">
-      <div class="col-md-6">
-        <img
-          src="https://picsum.photos/600/400"
-          alt="Event Image"
-          class="img-fluid rounded"
-        />
+  <div>
+    <div class="card">
+      <div class="container my-4">
+        <div class="row">
+          <div class="col-md-6">
+            <img
+              src="https://picsum.photos/600/400"
+              alt="Event Image"
+              class="img-fluid rounded"
+            />
+          </div>
+          <div class="col-md-6">
+            <h1 class="display-4">{{ event.name }}</h1>
+            <p class="lead">{{ event.description }}</p>
+            <p class="mb-2">
+              <strong>Event Time in {{ eventTz }}:</strong>
+              {{ getEventTime(event.utcTime, eventTz) }}
+            </p>
+            <p class="mb-2">
+              <strong>Your Time:</strong>
+              {{ getUserTime(event.utcTime) }}
+            </p>
+
+            <p v-if="ticketAvailable" class="mb-2">
+              <strong>Tickets Left:</strong> {{ event.ticketCount }}
+            </p>
+            <p v-if="!ticketAvailable" class="mb-2 text-danger">
+              <strong>Tickets sold out</strong>
+            </p>
+            <p class="mb-2"><strong>Price:</strong> ${{ event.price }}</p>
+            <p v-if="userStore.isAdmin" class="mb-2">
+              <strong>Budget:</strong> ${{ event.budget }}
+            </p>
+            <p v-if="userStore.isAdmin" class="mb-2">
+              <strong>Users with tickets:</strong> {{ event.users }}
+            </p>
+
+            <div class="mt-4">
+              <button
+                v-if="userStore.isAdmin"
+                @click="deleteEvent"
+                class="btn btn-danger m-2"
+              >
+                Delete Event
+              </button>
+              <button
+                v-if="userStore.isAdmin"
+                @click="editEvent"
+                class="btn btn-primary m-2"
+              >
+                Edit Event
+              </button>
+              <button
+                v-if="userStore.isAdmin"
+                @click="viewBudget"
+                class="btn btn-primary m-2"
+              >
+                View Budget
+              </button>
+              <button
+                v-if="
+                  userStore.user &&
+                  !userStore.isAdmin &&
+                  !event.users.includes(userStore.user.email) &&
+                  ticketAvailable
+                "
+                @click="buyTicket"
+                class="btn btn-primary m-2"
+              >
+                Buy a Ticket
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-md-6">
-        <h1 class="display-4">{{ event.name }}</h1>
-        <p class="lead">{{ event.description }}</p>
-        <p class="mb-2">
-          <strong>Event Time in {{ eventTz }}:</strong>
-          {{ getEventTime(event.utcTime, eventTz) }}
-        </p>
-        <p class="mb-2">
-          <strong>Your Time:</strong>
-          {{ getUserTime(event.utcTime) }}
-        </p>
-
-        <p v-if="ticketAvailable" class="mb-2">
-          <strong>Tickets Left:</strong> {{ event.ticketCount }}
-        </p>
-        <p v-if="!ticketAvailable" class="mb-2 text-danger">
-          <strong>Tickets sold out</strong>
-        </p>
-        <p class="mb-2"><strong>Price:</strong> ${{ event.price }}</p>
-        <p v-if="userStore.isAdmin" class="mb-2">
-          <strong>Budget:</strong> ${{ event.budget }}
-        </p>
-        <p v-if="userStore.isAdmin" class="mb-2">
-          <strong>Users with tickets:</strong> {{ event.users }}
-        </p>
-
-        <div class="mt-4">
-          <button
-            v-if="userStore.isAdmin"
-            @click="deleteEvent"
-            class="btn btn-danger m-2"
-          >
-            Delete Event
-          </button>
-          <button
-            v-if="userStore.isAdmin"
-            @click="editEvent"
-            class="btn btn-primary m-2"
-          >
-            Edit Event
-          </button>
-          <button
-            v-if="userStore.isAdmin"
-            @click="viewBudget"
-            class="btn btn-primary m-2"
-          >
-            View Budget
-          </button>
-          <button
-            v-if="
-              userStore.user &&
-              !userStore.isAdmin &&
-              !event.users.includes(userStore.user.email) &&
-              ticketAvailable
-            "
-            @click="buyTicket"
-            class="btn btn-primary m-2"
-          >
-            Buy a Ticket
-          </button>
+      <EditEventModal v-if="isEditing" />
+    </div>
+    <div class="card">
+      <div class="container my-4">
+        <div class="row">
+          <div class="col-md-6">
+          <p class="lead">Event Location</p>
+          <MapDisplay :location="event.location" />
+        </div>
+        <div class="col-md-6">
+          <p class="lead">Event Countdown</p>
+          <p>TODO</p>
+        </div>
         </div>
       </div>
     </div>
   </div>
-  <EditEventModal v-if="isEditing" />
 </template>
 
 <script setup>
@@ -86,6 +101,7 @@ import { useUserStore } from "@/store/userStore";
 import EditEventModal from "@/components/EditEventModal.vue";
 import { getUserTime, getEventTime } from "@/utils/transformTime.js";
 import convertCoordsToTz from "@/utils/getTzFromCoords.js";
+import MapDisplay from "@/components/MapDisplay.vue";
 
 const route = useRoute();
 const router = useRouter();
