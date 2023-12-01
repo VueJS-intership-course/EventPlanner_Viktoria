559f5f99fe880d787e650458aa8c6938612a6d23
Dimo Milkovski <dimo.milkovski@asml.com>
2023-12-01 15:21:31 +0200

    broken input

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="mb-3">
+<InputField :componentProps="{class: 'custom'}" />
+
+  <div class="mb-3" >
     <label :for="inputId" class="form-label">{{ label }}</label>
     <Field
-      v-if="type !== 'textarea'"
-      :type="type"
-      :id="inputId"
-      :name="inputId"
+      v-bind="fieldAttrs"
       :value="value"
       @input="updateInputValue"
       class="form-control"
     />
-    <Field
+    <!-- use computed property that determinate "as" for vee validate -->
+    <!-- <Field
       v-if="type === 'textarea'"
+_
       as="textarea"
       :id="inputId"
       :name="inputId"
       :value="value"
       @input="updateInputValue"
       class="form-control"
-    />
+    /> -->
     <ErrorMessage :name="inputId" class="text-danger" />
   </div>
 </template>
 
 <script setup>
 import { Field, ErrorMessage } from "vee-validate";
+import {computed} from 'vue';
 
 const props = defineProps({
+  inputAttrs: {
+    type: Object,
+    default: () => ({})
+  },
   label: String,
   type: String,
   inputId: String,
@@ -38,4 +44,15 @@ const emit = defineEmits(["update:modelValue"]);
 const updateInputValue = (event) => {
   emit("update:modelValue", event.target.value);
 };
+
+const fieldAttrs = computed(() => {
+
+  const attrs = {...props.inputAttrs.value}
+  if (props.type.value === 'textarea') {
+    attrs.as = 'textarea'
+  }
+
+  return attrs
+})
+
 </script>
